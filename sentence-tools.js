77fbedd625cbaaf7678143806a433c9b23d1749f
wooorm <tituswormer@gmail.com>
2014-12-06 16:11:38 +0100

    Add async support

--- sentence-tools.js
+++ sentence-tools.js
@@ -3,32 +3,32 @@
 var SINGLE_QUOTE = exports.SINGLE_QUOTE = "'";
 var DOUBLE_QUOTE = exports.DOUBLE_QUOTE = '"';
 
-exports.normalizeWhitespace = function (sentence) {
-  return sentence.replace(/\s+/g, ' ');
+exports.normalizeWhitespace = function (value, cb) {
+  cb(null, value.replace(/\s+/g, ' '));
 };
 
-exports.trim = function (sentence) {
-  return sentence.trim();
+exports.trim = function (value, cb) {
+  cb(null, value.trim());
 };
 
-exports.normalizeQuotes = function (text) {
-  return text
+exports.normalizeQuotes = function (value, cb) {
+  cb(null, value
     .replace(/''|``|’’|‛‛/g, DOUBLE_QUOTE)
     .replace(/'|‘|’|‚|‛|‹|›/g, SINGLE_QUOTE)
-    .replace(/"|“|”|„|‟|«|»/g, DOUBLE_QUOTE);
+    .replace(/"|“|”|„|‟|«|»/g, DOUBLE_QUOTE));
 };
 
-exports.compress = function (sentence) {
-  return sentence.replace(/[.]{2,3}/g, '…');
+exports.compress = function (value, cb) {
+  cb(null, value.replace(/[.]{2,3}/g, '…'));
 };
 
 // TODO: Improve this to know about acronyms
-exports.stripTrailingPeriod = function (sentence) {
-  return sentence.replace(/\.$/, '');
+exports.stripTrailingPeriod = function (value, cb) {
+  cb(null, value.replace(/\.$/, ''));
 };
 
-exports.capitalize = function (sentence) {
-  return sentence.charAt(0).toUpperCase() + sentence.substring(1);
+exports.capitalize = function (value, cb) {
+  cb(null, value.charAt(0).toUpperCase() + value.substring(1));
 };
 
 exports.tokenize = require('./lib/tokenize.js');
--- test/sentence-tools.js
+++ test/sentence-tools.js
@@ -8,39 +8,52 @@ var sentenceTools = require('..');
 
 describe('sentence-tools', function () {
   describe('normalizeWhitespace', function () {
-    it('should normalize whitespace', function () {
-      sentenceTools.normalizeWhitespace('I   \nam a \t\t  sentence with  weird   spaces')
-        .should.equal('I am a sentence with weird spaces');
+    it('should normalize whitespace', function (done) {
+      sentenceTools.normalizeWhitespace('I   \nam a \t\t  sentence with  weird   spaces', function (err, result) {
+        result.should.equal('I am a sentence with weird spaces');
+
+        done(err);
+      });
     });
   });
 
   describe('normalizeQuotes', function () {
-    it('should normalize quotes', function () {
-      var quotes = '‘’‛ \'\' `` ’’ “ ” „ ‟';
+    it('should normalize quotes', function (done) {
+      sentenceTools.normalizeQuotes('‘’‛ \'\' `` ’’ “ ” „ ‟', function (err, result) {
+        result.should.equal('\'\'\' " " " " " " "');
 
-      sentenceTools.normalizeQuotes(quotes)
-        .should.equal('\'\'\' " " " " " " "');
+        done(err);
+      });
     });
   });
 
   describe('compress', function () {
-    it('should compress a sentence', function () {
-      sentenceTools.compress("I don't know... Sounds scary..")
-        .should.equal("I don't know… Sounds scary…");
+    it('should compress a sentence', function (done) {
+      sentenceTools.compress("I don't know... Sounds scary..", function (err, result) {
+        result.should.equal("I don't know… Sounds scary…");
+
+        done(err);
+      });
     });
   });
 
   describe('capitalize', function () {
-    it('should capitalize the first letter of a sentence', function () {
-      sentenceTools.capitalize('lower case, huh?')
-        .should.equal('Lower case, huh?');
+    it('should capitalize the first letter of a sentence', function (done) {
+      sentenceTools.capitalize('lower case, huh?', function (err, result) {
+        result.should.equal('Lower case, huh?');
+
+        done(err);
+      });
     });
   });
 
   describe('trim', function () {
-    it('should trim whitespace from a sentence', function () {
-      sentenceTools.trim('  \t  testing  \t  ')
-        .should.equal('testing');
+    it('should trim whitespace from a sentence', function (done) {
+      sentenceTools.trim('  \t  testing  \t  ', function (err, result) {
+        result.should.equal('testing');
+
+        done(err);
+      });
     });
   });
 
@@ -48,7 +61,7 @@ describe('sentence-tools', function () {
     var sentencesFixture = require('./sentences.json');
     var article = fs.readFileSync(path.resolve(__dirname, 'article.txt'));
 
-    it('should tokenize a buffer into sentences', function (cb) {
+    it('should tokenize a buffer into sentences', function (done) {
       sentenceTools.tokenize(article, function (err, sentences) {
         if (err) {
           return cb(err);
@@ -56,11 +69,11 @@ describe('sentence-tools', function () {
 
         sentences.should.deep.equal(sentencesFixture);
 
-        cb();
+        done();
       });
     });
 
-    it('should tokenize a string into sentences', function (cb) {
+    it('should tokenize a string into sentences', function (done) {
       sentenceTools.tokenize(article.toString(), function (err, sentences) {
         if (err) {
           return cb(err);
@@ -68,7 +81,7 @@ describe('sentence-tools', function () {
 
         sentences.should.deep.equal(sentencesFixture);
 
-        cb();
+        done();
       });
     });
   });
